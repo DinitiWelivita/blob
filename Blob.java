323d783fe0492a79c32636edec5ecd4830fe80b3
DinitiWelivita <47827857+DinitiWelivita@users.noreply.github.com>
2020-11-18 09:43:03 -0500

    Add files via upload

--- Blob.java
+++ Blob.java
@@ -2,62 +2,63 @@ package sim;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Blob {
-    //fields
+    //Fields
     private int x, y, vx, vy, hp;
-    private final static int SIZE = 10, MAX_SPEED = 3;
+    private final static int SIZE = 25, MAX_SPEED = 10;
     private final static Color COLOR = Color.RED;
     
-    //constructors
+    //Constructor
     //overloaded
     public Blob(int x, int y) {
-        this.hp = 100;
         this.x = x;
         this.y = y;
-        this.vx = (int)((Math.random()*2*MAX_SPEED)-MAX_SPEED);
-        this.vy = (int)((Math.random()*2*MAX_SPEED)-MAX_SPEED);
+        this.vx = (int) (Math.random()*2*MAX_SPEED) - MAX_SPEED;
+        this.vy = (int) (Math.random()*2*MAX_SPEED) - MAX_SPEED;
     }
     
     //no-args constructor
     public Blob() {
-       this.x = (int)(Math.random()*600);
-       this.y = (int)(Math.random()*600);
-       this.vx = (int)((Math.random()*2*MAX_SPEED)-MAX_SPEED);
-        this.vy = (int)((Math.random()*2*MAX_SPEED)-MAX_SPEED); 
+        this.hp = 80;
+        this.x = (int) (Math.random()*600);
+        this.y = (int) (Math.random()*600);        
+        this.vx = (int) (Math.random()*2*MAX_SPEED) - MAX_SPEED;
+        this.vy = (int) (Math.random()*2*MAX_SPEED) - MAX_SPEED;
     }
-   
-    //methods
+    
+    //Methods
     public void move() {
-       x += vx;
-       y += vy;
-      
+        x += vx;
+        y += vy;
     }
     
     public void draw(Graphics g) {
-        g.setColor(COLOR);
-        g.fillOval(x,y,SIZE,SIZE);
+        Color color = new Color((int)(Math.random()*100+150),(int)(Math.random()*100+150),(int)(Math.random()*255),(int)(Math.random()*100+150) );
+        g.setColor(color);
+        g.fillOval(x, y, SIZE, SIZE);
     }
     
-//getters and setters
-
     public void collideWorldBounds(World w) {
-        if (x>w.getSIZE()|| x<0) {
+        if (x > w.getSIZE() || x < 0) {
             vx *= -1;
         }
-       if (y>w.getSIZE() || y<0) {
-           vy *= -1;
-       }
+        if (y > w.getSIZE() || y < 0) {
+            vy *= -1;
+        }
     }
     
     public void collide(Blob other) {
-        if (circleVsCircle(this, other)) {
-            if (this.hp>other.hp) {
+        if (circleVsCircle(this,other)) {
+            if (this.hp > other.hp) {
                 other.hp--;
+                
+
             }
-            
-            else if (this.hp<other.hp) {
+            else if (this.hp < other.hp) {
                 this.hp--;
+                
             }
             else {
                 this.hp--;
@@ -69,15 +70,16 @@ public class Blob {
             this.vy = other.vy;
             other.vx = tempvx;
             other.vy = tempvy;
-            
         }
     }
     
     private boolean circleVsCircle(Blob b1, Blob b2) {
-        if (dist(b1, b2)<Blob.SIZE) {
+        if (dist(b1,b2) < Blob.SIZE)
+        {
             return true;
         }
-        else {
+        else 
+        {
             return false;
         }
     }
@@ -85,11 +87,10 @@ public class Blob {
     private double dist(Blob b1, Blob b2) {
         return Math.sqrt(Math.pow(b2.x-b1.x,2)+Math.pow(b2.y-b1.y,2));
     }
+    //Getters & setters
 
     public int getHp() {
         return hp;
     }
-    
-    
     
 }
--- World.java
+++ World.java
@@ -6,33 +6,34 @@ import java.awt.Graphics;
 public class World {
     private final int SIZE = 600;
     private final Color bg = Color.GRAY;
-    //declare arrays of Blobs
-    private Blob[] blobs = new Blob[100];
+    //declare array of Blobs
+    private Blob[] blobs = new Blob[2000];
     
     public World() {
-        for(int i=0;i< blobs.length; i++) {
-            blobs [i] = new Blob();
-           
+        for (int i = 0; i < blobs.length; i++) {
+            blobs[i] = new Blob();
         }
     }
     
-    public void draw(Graphics g){
-        //for each loop, enhanced for loop
-        for(Blob blob:blobs) {
-            if (blob.getHp()<=0) continue;
+    public void draw(Graphics g) {
+       
+        //For each loop, enhanced for loop
+        for (Blob blob : blobs) {
+            if (blob.getHp() <= 0) continue;
             blob.move();
-            blob.draw(g);
             blob.collideWorldBounds(this);
             for (Blob b2 : blobs) {
+                 if (blob ==b2) continue;
+                 //continue means ignore
+                  if (b2.getHp() <= 0) continue;
                 blob.collide(b2);
             }
+            blob.draw(g);
         }
     }
 
     public int getSIZE() {
         return SIZE;
     }
-    
-    
     
 }
